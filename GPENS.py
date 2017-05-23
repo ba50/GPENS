9f8de87d8287fb766d4fa110032c9f30afb7d2a1
ba50 <barjas02@gmail.com>
2017-05-23 15:06:51 +0200

    praca nad wydajnoscia i spójnością kodu

--- GPENS.py
+++ GPENS.py
@@ -2,22 +2,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import animation
 
-def potential(x):
-    return 0.5*x*x
+
+def potential(x_):
+    return 0.5*x_**2
 
 
 class GPENS:
-    def __init__(self, x, psi, n, dt):
+    def __init__(self, x, psi, dt, end_time):
         self.x = x
+        self.psi = psi
+        self.dt = dt
+        self.end_time = end_time
+
         self.x_min = x[0]
         self.x_max = x[-1]
         self.dx = abs(x[1] - x[0])
-        self.dt = dt
 
         period = np.max(x) - np.min(x)
         self.dk = 2 * np.pi / period
-        self.k = -0.5 * n * self.dk + self.dk * np.arange(n)
-        self.psi = psi
+        self.k = -0.5 * self.x.size * self.dk + self.dk * np.arange(self.x.size)
 
         self.normalize(self.psi)
         self.normalize(self.psi)
@@ -25,21 +28,21 @@ class GPENS:
         self.normalize(self.psi)
         self.normalize(self.psi)
 
+        self.time_steps = int(self.end_time/self.dt)
+        self.energy = np.zeros((3, self.time_steps)).astype(np.float32)
         self.E_k = None
-        self.kinetic_energy()
         self.E_p = None
-        self.potential_energy()
+        self.E_tot = None
 
     def normalize(self, psi):
         # sum function using rectangles method
         n = np.sum(psi*np.conj(psi))*self.dx
         return psi/np.sqrt(n)
 
-    def kinetic_energy(self):
+    def update_energy(self):
         self.E_k = (np.sum(np.conj(self.psi)*-1j*np.gradient(self.psi)))**2/2
-
-    def potential_energy(self):
         self.E_p = np.sum(self.psi*np.conj(self.psi)*potential(self.x))*self.dx
+        self.E_tot = self.E_k + self.E_p
 
     def evolve_imaginary(self):
         # move to reciprocal space
@@ -78,18 +81,14 @@ class GPENS:
 
         return np.exp(1j*potential(self.x)*self.dt)*psi_x
 
-    def make_evolution(self, end_time):
-        time_steps = int(end_time/self.dt)
-        energy = np.zeros((3, time_steps), dtype=np.complex64)
-
+    def make_evolution(self):
         self.psi = self.evolve_imaginary()
-        for index in range(time_steps):
+        for index in range(self.time_steps):
             self.psi = self.evolve_imaginary()
-            self.kinetic_energy()
-            energy[0, index] = self.E_k
-            self.potential_energy()
-            energy[1, index] = self.E_p
-            energy[2, index] = self.E_k + self.E_p
+            self.update_energy()
+            self.energy[0, index] = self.E_k
+            self.energy[1, index] = self.E_p
+            self.energy[2, index] = self.E_tot
 
     def animation(self, steps, end_time):
         ims = []
@@ -104,10 +103,9 @@ class GPENS:
 x_min = -5.25
 x_max = +5.25
 Nx = 2048
-x = np.linspace(x_min, x_max, Nx)
-psi_x = np.exp(-0.5*x**2)*np.pi**(-0.25)
-test = GPENS(x, psi_x, Nx, 1e-4)
-
+x_temp = np.linspace(x_min, x_max, Nx)
+psi_x_temp = np.exp(-0.5*x_temp**2)*np.pi**(-0.25)
+test = GPENS(x_temp, psi_x_temp, Nx, 1e-4)
 
 fig = plt.figure()
 imp_ani = animation.ArtistAnimation(fig, test.animation(2, 1e-2), interval=50, repeat_delay=3000, blit=True)
